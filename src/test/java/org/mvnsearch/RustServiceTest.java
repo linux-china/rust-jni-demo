8567f7d0e1c128869ec4e3fe9386ebdca88f7684
linux_china <libing.chen@gmail.com>
2020-09-17 15:19:03 -0700

    RustService test

---
+++ src/test/java/org/mvnsearch/RustServiceTest.java
@@ -0,0 +1,12 @@
+package org.mvnsearch;
+
+
+import org.junit.jupiter.api.Test;
+
+public class RustServiceTest {
+
+    @Test
+    public void testHello() {
+        System.out.println(RustService.hello("Jackie"));
+    }
+}
