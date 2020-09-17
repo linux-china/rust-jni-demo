08af636640d3d4e8226251acd86f02c7910c74a0
linux_china <libing.chen@gmail.com>
2020-09-17 14:48:46 -0700

    RustService class

---
+++ src/main/java/org/mvnsearch/RustService.java
@@ -0,0 +1,21 @@
+package org.mvnsearch;
+
+/**
+ * Rust Service by JNI
+ *
+ * @author linux_china
+ */
+public class RustService {
+    static {
+        System.loadLibrary("mylib");
+    }
+
+    /**
+     * static hello function which is provided by a native library.
+     *
+     * @param name input
+     * @return welcome
+     */
+    public static native String hello(String name);
+
+}
