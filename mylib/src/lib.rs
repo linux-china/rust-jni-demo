66121aa7ef092881cf0e68bb24924e1d673e2356
Libing Chen <libing.chen@gmail.com>
2023-02-27 18:56:33 +0800

    chore: api adjusted

--- mylib/src/lib.rs
+++ mylib/src/lib.rs
@@ -15,18 +15,18 @@ use libc::{c_char, c_int};
 // This is the class that owns our static method. It's not going to be used,
 // but still must be present to match the expected signature of a static native method.
 #[no_mangle]
-pub extern "system" fn Java_org_mvnsearch_RustService_hello(env: JNIEnv,
+pub extern "system" fn Java_org_mvnsearch_RustService_hello(mut env: JNIEnv,
                                                             _class: JClass,
                                                             name: JString)
                                                             -> jstring {
     // convert Java's type to Rust's type
-    let name: String = env.get_string(name).expect("Couldn't get java string!").into();
+    let name: String = env.get_string(&name).expect("Couldn't get java string!").into();
     println!("Received: {}", &name);
     // call Rust native function
     let result = hello(&name);
 
     // Finally, extract the raw pointer to return.
-    env.new_string(result).expect("Couldn't create java string!").into_inner()
+    env.new_string(result).expect("Couldn't create java string!").as_raw()
 }
 
 fn hello(name: &str) -> String {
@@ -34,11 +34,11 @@ fn hello(name: &str) -> String {
 }
 
 #[no_mangle]
-pub extern "system" fn Java_org_mvnsearch_RustService_count(env: JNIEnv,
+pub extern "system" fn Java_org_mvnsearch_RustService_count(mut env: JNIEnv,
                                                             _class: JClass,
                                                             name: JString)
                                                             -> i32 {
-    let name: String = env.get_string(name).expect("Couldn't get java string!").into();
+    let name: String = env.get_string(&name).expect("Couldn't get java string!").into();
     println!("Received: {}", &name);
     name.len() as i32
 }
@@ -57,6 +57,7 @@ pub extern "C" fn Java_org_mvnsearch_RustService_echo(s: *const c_char) -> c_int
         CStr::from_ptr(s)
     };
     let r_str = c_str.to_str().unwrap();
+    println!("Received: {}", r_str);
     r_str.chars().count() as c_int
 }
 
