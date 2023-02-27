a1c247c67607d3cb5b154ed5385f84b3afc6f9fe
Libing Chen <libing.chen@gmail.com>
2023-02-27 23:14:18 +0800

    chore: remove libc

--- mylib/src/lib.rs
+++ mylib/src/lib.rs
@@ -1,23 +1,17 @@
-use std::ffi::CStr;
-
 // This is the interface to the JVM that we'll call the majority of our methods on.
 use jni::JNIEnv;
 // These objects are what you should use as arguments to your native
 // function. They carry extra lifetime information to prevent them escaping
 // this context and getting used after being GC'd.
 use jni::objects::{JClass, JString};
-// This is just a pointer. We'll be returning it from our function. We
-// can't return one of the objects with lifetime information because the
-// lifetime checker won't let us.
-use libc::{c_char, c_int};
 
 // This is the class that owns our static method. It's not going to be used,
 // but still must be present to match the expected signature of a static native method.
 #[no_mangle]
 pub extern "system" fn Java_org_mvnsearch_RustService_hello<'local>(mut env: JNIEnv<'local>,
-                                                            _class: JClass<'local>,
-                                                            name: JString<'local>)
-                                                            -> JString<'local> {
+                                                                    _class: JClass<'local>,
+                                                                    name: JString<'local>)
+                                                                    -> JString<'local> {
     // convert Java's type to Rust's type
     let name: String = env.get_string(&name).expect("Couldn't get java string!").into();
     println!("Received: {}", &name);
@@ -40,24 +34,6 @@ pub extern "system" fn Java_org_mvnsearch_RustService_count(mut env: JNIEnv,
     let name: String = env.get_string(&name).expect("Couldn't get java string!").into();
     println!("Received: {}", &name);
     name.len() as i32
-}
-
-
-// Rust FFI standard
-#[no_mangle]
-pub extern "C" fn Java_org_mvnsearch_RustService_increase(base: c_int) -> c_int {
-    base + 1
-}
-
-#[no_mangle]
-pub extern "C" fn Java_org_mvnsearch_RustService_echo(s: *const c_char) -> c_int {
-    let c_str = unsafe {
-        assert!(!s.is_null());
-        CStr::from_ptr(s)
-    };
-    let r_str = c_str.to_str().unwrap();
-    println!("Received: {}", r_str);
-    r_str.chars().count() as c_int
 }
 
 
